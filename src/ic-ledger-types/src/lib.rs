3a0287489b27cc1b4d9b54544dd93306e97fc8d7
Hamish Peebles <hamishpeebles@gmail.com>
2022-04-06 15:02:05 +0100

    Integrate with new `query_blocks` method

--- src/ic-ledger-types/src/lib.rs
+++ src/ic-ledger-types/src/lib.rs
@@ -246,6 +246,124 @@ impl fmt::Display for TransferError {
     }
 }
 
+#[derive(Serialize, Deserialize, CandidType)]
+pub struct GetBlocksArgs {
+    pub start: BlockIndex,
+    pub length: usize,
+}
+
+pub type GetBlocksResult = Result<BlockRange, GetBlocksError>;
+
+#[derive(Serialize, Deserialize, CandidType, Debug)]
+pub struct BlockRange {
+    pub blocks: Vec<Block>,
+}
+
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize, CandidType)]
+pub enum GetBlocksError {
+    BadFirstBlockIndex {
+        requested_index: BlockIndex,
+        first_valid_index: BlockIndex,
+    },
+    Other {
+        error_code: u64,
+        error_message: String,
+    },
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(try_from = "candid::types::reference::Func")]
+pub struct QueryArchiveFn {
+    pub canister_id: Principal,
+    pub method: String,
+}
+
+impl From<QueryArchiveFn> for candid::types::reference::Func {
+    fn from(archive_fn: QueryArchiveFn) -> Self {
+        Self {
+            principal: archive_fn.canister_id,
+            method: archive_fn.method,
+        }
+    }
+}
+
+impl TryFrom<candid::types::reference::Func> for QueryArchiveFn {
+    type Error = String;
+    fn try_from(func: candid::types::reference::Func) -> Result<Self, Self::Error> {
+        Ok(QueryArchiveFn {
+            canister_id: func.principal,
+            method: func.method,
+        })
+    }
+}
+
+impl CandidType for QueryArchiveFn {
+    fn _ty() -> candid::types::Type {
+        candid::types::Type::Func(candid::types::Function {
+            modes: vec![candid::parser::types::FuncMode::Query],
+            args: vec![GetBlocksArgs::_ty()],
+            rets: vec![GetBlocksResult::_ty()],
+        })
+    }
+
+    fn idl_serialize<S>(&self, serializer: S) -> Result<(), S::Error>
+        where
+            S: candid::types::Serializer,
+    {
+        candid::types::reference::Func::from(self.clone()).idl_serialize(serializer)
+    }
+}
+
+#[derive(Debug, CandidType, Deserialize)]
+pub struct ArchivedBlocksRange {
+    pub start: BlockIndex,
+    pub length: u64,
+    pub callback: QueryArchiveFn,
+}
+
+/// An operation which modifies account balances
+#[derive(Serialize, Deserialize, CandidType, Clone, Hash, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Operation {
+    Burn {
+        from: AccountIdentifier,
+        amount: Tokens,
+    },
+    Mint {
+        to: AccountIdentifier,
+        amount: Tokens,
+    },
+    Transfer {
+        from: AccountIdentifier,
+        to: AccountIdentifier,
+        amount: Tokens,
+        fee: Tokens,
+    },
+}
+
+/// An operation with the metadata the client generated attached to it
+#[derive(Serialize, Deserialize, CandidType, Clone, Hash, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Transaction {
+    pub operation: Operation,
+    pub memo: Memo,
+    pub created_at_time: Timestamp,
+}
+
+#[derive(CandidType, Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
+pub struct Block {
+    pub parent_hash: Option<[u8; 32]>,
+    pub transaction: Transaction,
+    pub timestamp: Timestamp,
+}
+
+#[derive(Debug, CandidType, Deserialize)]
+pub struct QueryBlocksResponse {
+    pub chain_length: u64,
+    pub certificate: Option<serde_bytes::ByteBuf>,
+    pub blocks: Vec<Block>,
+    pub first_block_index: BlockIndex,
+    pub archived_blocks: Vec<ArchivedBlocksRange>,
+}
+
 /// Calls the "account_balance" method on the specified canister.
 ///
 /// # Example
@@ -295,6 +413,30 @@ pub async fn transfer(
     args: TransferArgs,
 ) -> CallResult<TransferResult> {
     let (result,) = ic_cdk::call(ledger_canister_id, "transfer", (args,)).await?;
+    Ok(result)
+}
+
+/// Calls the "query_blocks" method on the specified canister.
+/// # Example
+/// ```no_run
+/// use ic_cdk::api::{caller, call::call};
+/// use ic_ledger_types::{Block, BlockIndex, GetBlocksArgs, MAINNET_LEDGER_CANISTER_ID, query_blocks};
+///
+/// async fn get_blocks_since(block_index: BlockIndex, max_blocks: usize) -> Vec<Block> {
+///   query_blocks(
+///     MAINNET_LEDGER_CANISTER_ID,
+///     GetBlocksArgs {
+///         start: block_index,
+///         length: max_blocks,
+///     }
+///   ).await.expect("call to ledger failed").blocks
+/// }
+/// ```
+pub async fn query_blocks(
+    ledger_canister_id: Principal,
+    args: GetBlocksArgs,
+) -> CallResult<QueryBlocksResponse> {
+    let (result,) = ic_cdk::call(ledger_canister_id, "query_blocks", (args,)).await?;
     Ok(result)
 }
 
