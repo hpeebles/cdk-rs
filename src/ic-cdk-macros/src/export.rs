bb37ab8d75b73b18e92ed716402ff554aeb4501d
Hamish Peebles <hamishpeebles@gmail.com>
2022-05-24 16:00:47 +0100

    Act on slice of bytes rather than Vec

--- src/ic-cdk-macros/src/export.rs
+++ src/ic-cdk-macros/src/export.rs
@@ -176,7 +176,7 @@ fn dfn_macro(
         quote! {}
     } else if let Some(deserializer) = attrs.deserializer {
         let deserializer_ident = syn::Ident::new(&deserializer, Span::call_site());
-        quote! { let ( #( #arg_tuple, )* ) = #deserializer_ident (ic_cdk::api::call::arg_data_raw()); }
+        quote! { let ( #( #arg_tuple, )* ) = #deserializer_ident (&ic_cdk::api::call::arg_data_raw()); }
     } else {
         quote! { let ( #( #arg_tuple, )* ) = ic_cdk::api::call::arg_data(); }
     };
